a37570a77b936b103236549c2d5bfb2f123457cb
vn-ladm-vinhpt <vn-ladm-vinhpt@VN-L0560.hcg.homecredit.net>
2019-12-08 12:46:38 +0700

    Initial skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'airbnb'],
+	parser: 'babel-eslint',
+	env: {
+  	jest: true,
+	},
+	rules: {},
+	globals: {
+  	fetch: false,
+	}
+};
