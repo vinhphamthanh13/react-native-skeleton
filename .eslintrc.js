bb863ce38cc0126051bc8093cb06ee818b3eba09
David Pham <david.vinhphamthanh16@gmail.com>
2019-12-11 14:09:20 +0700

    Add more configurations for editorconfig and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'airbnb'],
+  extends: ['@react-native-community'], // airbnb
 	parser: 'babel-eslint',
 	env: {
   	jest: true,
